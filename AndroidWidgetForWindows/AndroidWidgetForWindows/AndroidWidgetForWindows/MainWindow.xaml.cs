9eb2a59411dff2c2168b819d61a8253fa56801e5
hakumai22 <coro501wa@outlook.jp>
2024-01-19 15:48:48 +0900

    zip extract

---
+++ AndroidWidgetForWindows/AndroidWidgetForWindows/AndroidWidgetForWindows/MainWindow.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace AndroidWidgetForWindows
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        
+        public MainWindow()
+        {
+            InitializeComponent();
+            Page1 = new Page1();
+            Uri1 = new Uri("Page1.xaml", UriKind.Relative);
+        }
+        public Uri Uri1 { get; set; }
+        public Page1 Page1 { get; }
+
+        private void Window_ContentRendered(object sender, EventArgs e)
+        {
+            Frame1.Navigate(Uri1);
+        }
+    }
+}
